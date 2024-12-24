d0b4b83b816eed766a84e74cffb10b1acca87113
empathyxo <deathxpunk676@gmail.com>
2024-12-24 17:07:48 +0300

    sprint_08 подготовка файлов

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by empathyxo on 09.12.2024.
+//
+
+import Foundation
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    
+    static let reuseIdentifier: String = "ImagesListCell"
+}
